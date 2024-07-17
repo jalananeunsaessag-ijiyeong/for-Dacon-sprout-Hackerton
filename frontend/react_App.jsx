d99ed474b30dc14aa221ae1ae704040f36655673
Jiyoung0219 <128336150+Jiyoung0219@users.noreply.github.com>
2024-07-17 17:26:27 +0900

    Create react_App.jsx

---
+++ frontend/react_App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Chatbot from "./Chatbot";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>너를 위한 상담 서비스</h1>
+      </header>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
