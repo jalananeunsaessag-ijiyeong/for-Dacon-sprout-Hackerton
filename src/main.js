78b4e037690e147174e2323f5b5bfa599624c858
yoonie03 <128327967+yoonie03@users.noreply.github.com>
2024-07-17 15:22:53 +0900

    de

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
 
+=======
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, 지윤허팀의 ㅎㅅㅎ!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
+>>>>>>> c2064d8 (add main.js again)
