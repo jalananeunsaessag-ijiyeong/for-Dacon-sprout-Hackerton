719451843fe266f074a7269a348ef4798692fa68
yoonie03 <128327967+yoonie03@users.noreply.github.com>
2024-07-17 14:20:50 +0900

    add React file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
