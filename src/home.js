4995b39481bcd2bdac371320aab46282bed9a4f4
yoonie03 <128327967+yoonie03@users.noreply.github.com>
2024-07-17 15:31:46 +0900

    rerere

--- src/home.js
+++ src/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, 지윤허팀의 ㅎㅅㅎ!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
